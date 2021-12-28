1ac223102bcb0f71a057575139224029b34bc714
Karthik <kskar7hik@gmail.com>
2021-12-28 18:41:45 +0000

    comm

--- app.js
+++ app.js
@@ -84,10 +84,8 @@ const url="https://nyaa.si/view/1468972"
         const text = convert(html);
         var txt = $('#collapse-comments').contents().map(function() {
                 if($(this).html()){
-                const scrap= $(this).html().replace(/(\r\n|\n|\r)/gm, "").replace(/(\r\t|\t|\r)/gm, "");
-                const user=scrap.substring(scrap.indexOf('"User">')+7,scrap.indexOf('<',scrap.indexOf('"User">')));
+                const user=$(this).find('div[title="User]').text()
                 const comment=$(this).find('div[class="comment-content"]').text();
-                //scrap.substring(scrap.indexOf('<',scrap.indexOf('class="comment-content'))+1,scrap.indexOf('</div>',scrap.indexOf('class="comment-content'))-1);
                 return {user,comment};
                 }
         }).get()
