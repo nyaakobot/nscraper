03f56f99254aec9ba83348cbcbf96c3041600027
Karthik <kskarthik20025@gmail.com>
2022-01-26 21:25:16 +0530

    added filters

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app = express();
+const app=express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const axios = require("axios");
@@ -10,76 +10,75 @@ app.use(express.json());
 app.use('/', express.static(path.join(__dirname)));
 app.use(bodyParser.json());
 app.post('/api/results', async (req, res) => {
-    try {
+    try{
         console.log(req.body)
-        const { query, sortBy, order, filter } = req.body
-        var url = null;
-        const nquery = query.replace(/\s+/g, '+');
-        if (sortBy)
-            url = "https://nyaa.si/?f=0&c=0_0&q=" + nquery + "&s=" + sortBy + "&o=" + order;
+        const { query,sortBy,order,filters } = req.body
+        var url=null;
+        const nquery=query.replace(/\s+/g,'+');
+        if(sortBy)
+        url="https://nyaa.si/?f=0&c=0_0&q="+nquery+"&s="+sortBy+"&o="+order;
         else
-            url = "https://nyaa.si/?f=0&c=0_0&q=" + nquery
-        if (filter) url = url.replace('0_0', filter);
-        console.log(url)
+        url="https://nyaa.si/?f=0&c=0_0&q="+nquery
+        if (filters) url = url.replace('0_0', filters);
         const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
+	    const $ = cheerio.load(data);
         const tabl = $(".table-responsive table tbody tr");
-        const fetched = { results: [] };
-        tabl.each(function (idx, el) {
-            const row = $(el).children("td");
-            const arr = [];
-            row.each(function (idx, el2) {
-                var temp = $(el2).html().replace(/(\r\n|\n|\r)/gm, "").replace(/(\r\t|\t|\r)/gm, "");
-                if (temp.trim().length != 0) {
-                    arr.push(temp);
-                }
+        const fetched={results: []};
+            tabl.each(function(idx, el){
+                    const row= $(el).children("td");
+                    const arr=[];
+                    row.each(function(idx, el2){
+                        var temp=$(el2).html().replace(/(\r\n|\n|\r)/gm, "").replace(/(\r\t|\t|\r)/gm, "");
+                        if(temp.trim().length!=0){
+                            arr.push(temp);		
+                        }			
+                    });
+                    var i1=arr[1].indexOf("title=\"",arr[1].indexOf("fa fa-comments"))+7;
+                    var i2=arr[1].indexOf("\"",i1+2);
+                    const title=arr[1].substring(i1,i2);
+                    var i1=arr[0].indexOf("title=\"")+7;
+                    const dl="http://nyaa.si"+arr[2].substring(9,arr[2].indexOf("\"",11));
+                    const ml=arr[2].substring(arr[2].indexOf("magnet:"),arr[2].indexOf("\"",arr[2].indexOf("magnet:")+11));
+                    const size=arr[3];
+                    const dateAdded=arr[4];
+                    const seeds=arr[5];
+                    const leechers=arr[6];
+                    const id=dl.substring(dl.indexOf("download")+9,dl.indexOf(".torrent"))
+                    const result={id:id,title: title,	dlink: dl,mlink: ml,size: size,dateAdded: dateAdded,seeders: seeds,leechers: leechers};
+                    fetched.results.push(result);
             });
-            var i1 = arr[1].indexOf("title=\"", arr[1].indexOf("fa fa-comments")) + 7;
-            var i2 = arr[1].indexOf("\"", i1 + 2);
-            const title = arr[1].substring(i1, i2);
-            var i1 = arr[0].indexOf("title=\"") + 7;
-            const dl = "http://nyaa.si" + arr[2].substring(9, arr[2].indexOf("\"", 11));
-            const ml = arr[2].substring(arr[2].indexOf("magnet:"), arr[2].indexOf("\"", arr[2].indexOf("magnet:") + 11));
-            const size = arr[3];
-            const dateAdded = arr[4];
-            const seeds = arr[5];
-            const leechers = arr[6];
-            const id = dl.substring(dl.indexOf("download") + 9, dl.indexOf(".torrent"))
-            const result = { id: id, title: title, dlink: dl, mlink: ml, size: size, dateAdded: dateAdded, seeders: seeds, leechers: leechers };
-            fetched.results.push(result);
-        });
-        console.log(fetched.results.length + " results fetched");
-        res.json({ status: 'ok', data: fetched.results });
-        console.log("response sent")
-    } catch (e) {
-        console.error(e);
-        res.json({ status: 'error' });
-    }
+            console.log(fetched.results.length+" results fetched");
+            res.json({ status: 'ok', data: fetched.results });
+            console.log("response sent")
+        }catch (e) {
+            console.error(e);
+            res.json({status: 'error'});
+        }
 });
-app.post('/api/torrentData', async (req, res) => {
-    try {
-        const { id } = req.body;
-        const url = "https://nyaa.si/view/" + id;
+app.post('/api/torrentData',async (req,res)=>{
+    try{
+        const {id}=req.body;        
+        const url="https://nyaa.si/view/"+id;
         const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
-        var html = $('#torrent-description').html();
+	    const $ = cheerio.load(data);
+        var html =$('#torrent-description').html();
         const text = convert(html);
-        var txt = $('#collapse-comments').contents().map(function () {
-            if ($(this).html()) {
-                const scrap = $(this).html().replace(/(\r\n|\n|\r)/gm, "").replace(/(\r\t|\t|\r)/gm, "");
-                const user = scrap.substring(scrap.indexOf('"User">') + 7, scrap.indexOf('<', scrap.indexOf('"User">')));
-                const comment = $(this).find('div[class="comment-content"]').text();
-                return { user, comment };
+        var txt = $('#collapse-comments').contents().map(function() {
+            if($(this).html()){
+            const scrap= $(this).html().replace(/(\r\n|\n|\r)/gm, "").replace(/(\r\t|\t|\r)/gm, "");
+            const user=scrap.substring(scrap.indexOf('"User">')+7,scrap.indexOf('<',scrap.indexOf('"User">')));
+            const comment=$(this).find('div[class="comment-content"]').text();
+            return {user,comment};
             }
         }).get()
-        res.json({ status: 'ok', description: text, comments: txt })
+        res.json({status:'ok', description: text, comments: txt})
         console.log("response sent")
-    }
-    catch (e) {
-        console.error(e);
-        res.json({ status: 'error' })
-    }
+        }
+        catch (e) {
+            console.error(e);
+            res.json({status:'error'})
+        }
 })
 app.listen(process.env.PORT || PORT, () => {
-    console.log("listening " + PORT + "...");
+	console.log("listening "+PORT+"...");
 });
